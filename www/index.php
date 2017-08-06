3f7b6ddb0bcbedf17da5dd4ff04da75ce20f09bf
EC2 Default User <ec2-user@ip-172-31-3-34.us-west-2.compute.internal>
2017-08-06 08:26:12 +0000

    build 5

--- www/index.php
+++ www/index.php
@@ -18,6 +18,8 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<?php echo "My first PHP script!"; ?>
+
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is AWS linux server<?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
